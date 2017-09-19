39f3e9f790d20e4bf1baeb389945b34c73e89f50
mattcrowe5 <crowe.matthew5@gmail.com>
2017-09-19 14:11:37 -0400

    built count method

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,11 @@
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  array.each do |item|
+    if !new_hash[item]
+      new_hash[item] = 1
+    elsif new_hash[item]
+      new_hash[item] +=1
+    end
+  end
+  new_hash
 end
- 
